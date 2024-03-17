94d5e5aa1f5a5301533eb87f4de70049a9194756
Conlan Cesar <conlanc@gmail.com>
2024-03-17 17:24:01 -0400

    Openstack provider configuration

--- terraform/dist-cc/provider.tf
+++ terraform/dist-cc/provider.tf
@@ -1,5 +1,21 @@
 terraform {
   required_providers {
+    gitlab = {
+      source  = "gitlabhq/gitlab"
+      version = "~> 16.8.1"
+    }
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.53.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.2.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4.2"
+    }
     zerotier = {
       source  = "zerotier/zerotier"
       version = "~> 1.4"
@@ -9,4 +25,11 @@ terraform {
 
 provider "zerotier" {
   zerotier_central_token = var.zerotier_central_token
+}
+
+provider "openstack" {
+  user_name   = var.openstack_username
+  tenant_id   = var.openstack_project_id
+  password    = var.openstack_password
+  auth_url    = var.openstack_auth_url
 }
--- terraform/dist-cc/variables.tf
+++ terraform/dist-cc/variables.tf
@@ -2,3 +2,27 @@ variable "zerotier_central_token" {
   sensitive = true
   type      = string
 }
+
+variable "gitlab_ssh_user_ids" {
+  type = set(string)
+  default = [ "90579" ]
+}
+
+# Openstack Credentials
+variable "openstack_auth_url" {
+  type = string
+}
+
+variable "openstack_project_id" {
+  type = string
+  sensitive = true
+}
+
+variable "openstack_username" {
+  type = string
+}
+
+variable "openstack_password" {
+  type = string
+  sensitive = true
+}
