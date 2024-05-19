d94b3074ee6dca7e8240bfc86a3fe4c5a55e7d3a
Conlan Cesar <conlanc@gmail.com>
2024-05-18 22:05:09 -0400

    Cloudinit tweaks

--- terraform/dist-cc/cloudinit.tf
+++ terraform/dist-cc/cloudinit.tf
@@ -1,15 +1,34 @@
+data "gitlab_user_sshkeys" "user_ssh_keys" {
+  for_each = toset(var.gitlab_ssh_user_ids)
+  user_id  = each.value
+}
+
+data "template_file" "kairos_master" {
+  template = "${file("${path.module}/cloud-init/kairos_master.yaml")}"
+
+  vars = {}
+}
+
 data "cloudinit_config" "kairos_master" {
   gzip          = false
   base64_encode = false
 
   part {
     content_type = "text/cloud-config"
-    content = file("${path.module}/cloud-init/kairos_master.yaml")
+    content = data.template_file.kairos_master.rendered
   }
 
   part {
     content_type = "text/cloud-config"
-    content = file("${path.module}/cloud-init/kairos.yaml")
+    content = data.cloudinit_config.kairos_worker.rendered
+  }
+}
+
+data "template_file" "kairos_worker" {
+  template = "${file("${path.module}/cloud-init/kairos.yaml")}"
+
+  vars = {
+    ssh_keys = yamlencode(toset([for keys in flatten([for user in data.gitlab_user_sshkeys.user_ssh_keys : user.keys]) : keys.key]))
   }
 }
 
@@ -19,6 +38,10 @@ data "cloudinit_config" "kairos_worker" {
 
   part {
     content_type = "text/cloud-config"
-    content = file("${path.module}/cloud-init/kairos.yaml")
+    content = data.template_file.kairos_worker.rendered
   }
+}
+
+output "kairos_master_cloudinit" {
+  value = data.cloudinit_config.kairos_master.rendered 
 }
