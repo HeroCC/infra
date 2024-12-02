fd86e1e9e118fcb587ab69e680dd4cf8221892cd
Conlan Cesar <conlanc@gmail.com>
2024-12-01 20:51:01 -0500

    Add static route to Metadata server
    This is what `inet` does. Doesn't work currently, but likely is necessary for the end solution.

--- terraform/dist-cc/network.tf
+++ terraform/dist-cc/network.tf
@@ -20,6 +20,10 @@ data "openstack_networking_network_v2" "csail_float" {
   name = "FLOAT"
 }
 
+data "openstack_networking_network_v2" "csail_inet" {
+  name = "inet"
+}
+
 resource "openstack_networking_router_v2" "csail_ccdist_router" {
   name           = "kube-router"
   admin_state_up = true
@@ -35,8 +39,15 @@ resource "openstack_networking_subnet_v2" "csail_ccdist_network_subnet" {
   name       = "subnet_1"
   network_id = openstack_networking_network_v2.csail_ccdist_network.id
   cidr       = "192.168.199.0/24"
-  dns_nameservers = ["8.8.8.8"]
+  dns_nameservers = ["128.30.2.25", "128.30.2.23", "128.30.2.24"]
   ip_version = 4
+}
+
+resource "openstack_networking_subnet_route_v2" "csail_ccdist_network_subnet_route" {
+  subnet_id        = openstack_networking_subnet_v2.csail_ccdist_network_subnet.id
+  destination_cidr = "169.254.169.254/32"
+  // Fixes routing to the metadata service. Provided by `cloud-net` subnet of `inet`
+  next_hop         = "128.52.160.134"
 }
 
 resource "openstack_networking_router_interface_v2" "attach_router_to_subnet" {
