f9f7213036c8f61e0128001dd05da053d6885147
Conlan Cesar <conlanc@gmail.com>
2024-12-01 23:01:22 -0500

    Remove old image installations

--- terraform/dist-cc/compute.tf
+++ terraform/dist-cc/compute.tf
@@ -13,15 +13,3 @@ resource "openstack_compute_instance_v2" "csail_ccdist_master" {
     "csail" = "true"
   }
 }
-
-# resource "openstack_blockstorage_volume_v3" "kairos_install_volume" {
-#   name = "kairos-install-volume"
-#   size = 4
-#   image_id = openstack_images_image_v2.kairos_ubuntu_24.id
-# }
-
-# resource "openstack_compute_volume_attach_v2" "kairos_install_attachment" {
-#   instance_id = openstack_compute_instance_v2.csail_ccdist_master.id
-#   volume_id = openstack_blockstorage_volume_v3.kairos_install_volume.id
-#   device = "/dev/vdb"
-# }
